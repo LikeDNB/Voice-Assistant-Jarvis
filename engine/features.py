58c7ee38ba7c4a9f3d45cd58ead170706d7d196c
LikeDNB <lex1400@gmx.de>
2024-07-30 02:43:15 +0200

    day 3

---
+++ engine/features.py
@@ -0,0 +1,7 @@
+from playsound import playsound
+
+
+# Play assistant sound function
+def playAssistantSound():
+    music_dir = "www\\assets\\audio\\start_sound.wav"
+    playsound(music_dir)
--- www/main.js
+++ www/main.js
@@ -8,7 +8,36 @@ $(document).ready(function () {
         },
         out:{
             effect: "bounceOut",
+        },
+    });
+    
+
+    //Siri Config
+
+    var siriWave = new SiriWave({
+    container: document.getElementById("siri-container"),
+    width: 800,
+    height: 200,
+    style: "ios9",
+    amplitude: "1",
+    speed: "0.15",
+    autostart: true
+
+    });
+
+    //Siri message animation
+
+    $('.siri-message').textillate({
+        loop: true,
+        sync: true,
+        in:{
+            effect: "fadeInUp",
+            sync: true,
+        },
+        out:{
+            effect: "fadeOutUp",
+            sync: true,
         }
     })
-    
+
 });
