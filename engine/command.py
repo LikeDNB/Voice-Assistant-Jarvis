84f5c8907b3a8aca06b18c9118ad7e33700de6e1
LikeDNB <lex1400@gmx.de>
2024-07-30 03:55:52 +0200

    day 5

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 
 def speak(text):
@@ -6,9 +7,34 @@ def speak(text):
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[1].id)
     engine.setProperty('rate', 150)     # setting up new voice rate
-    print(voices)
+    # print(voices)
     engine.say(text)
     engine.runAndWait()
 
 
-speak("India is my country, all indians are my brothers and sisters")
+def takecommand():
+
+    r = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("listening.....")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+
+        audio = r.listen(source, 10, 6)
+
+    try:
+        print("recognizing")
+        query = r.recognize_google(audio, language='ru-RU')
+        print(f"user said: {query}")
+
+    except Exception as e:
+        return f"{e}"
+
+    return query.lower()
+
+
+text = takecommand()
+
+
+speak(text)
